4fa0b3062cc14d92f23f1ef552cc6b1cdd07ebf2
Nikita Mitasov <me@ch4og.com>
2026-06-23 22:17:48 +0300

    Fix invoice expiry date using Moscow timezone instead of server TZ
    Paykeeper expects dates in MSK. When server is in UTC and it's late
    evening, the expiry date was already in the past from Paykeeper's
    perspective, causing 'Дата выставляемого счёта уже прошла'.

--- pretix_paykeeper/payment.py
+++ pretix_paykeeper/payment.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 from datetime import timedelta
 from decimal import Decimal
 from urllib.parse import urljoin
+from zoneinfo import ZoneInfo
 
 import requests as http_requests
 from django import forms
@@ -271,7 +272,7 @@ class PaykeeperPaymentProvider(BasePaymentProvider):
 
     def _create_invoice(self, order, payment):
         token = self._get_token()
-        expiry_date = (now() + timedelta(hours=self._get_expiry_hours())).strftime('%Y-%m-%d')
+        expiry_date = (now().astimezone(ZoneInfo('Europe/Moscow')) + timedelta(hours=self._get_expiry_hours())).strftime('%Y-%m-%d')
         order_id = '{}-{}-{}'.format(order.event.slug, order.code, payment.pk)
 
         callback_url = build_absolute_uri(
